1a4b2e6cdfd335c124634bf324dbd33be7f524b4
Harald Hartmann <harald@hartmann-dev.net>
2020-09-19 19:26:59 +0200

    Faktor für die Größen eingebaut

--- constants/Dimensions.js
+++ constants/Dimensions.js
@@ -1,13 +1,41 @@
+import { Dimensions } from 'react-native'
+
+const winDim = Dimensions.get("window");
+let factor = 1;
+
+const space = Math.min(winDim.width, winDim.height);
+ 
+console.log( "space: "  + space);
+
+if(space < 1400)
+ factor = 3.2;  
+
+if(space < 1030)
+ factor = 2.5; 
+
+if(space < 840)
+ factor = 2.2;
+
+if(space < 420)
+ factor = 1.7; 
+ 
+ if(space < 380)
+ factor = 1.5; 
+
+if(space < 350)
+ factor = 1.2; 
+
+
 export default {
     home: {
-        height: 300,
-        width: 300,
-        margin: 15
+        height: 100 * factor,
+        width:  100 * factor,
+        margin: 15 
     },
-    gallery: {
-        height: 220,
-        width: 220,
-        margin: 15
-    }
-};
-  
+    gallery: { 
+        height: 60 * factor,
+        width: 60 * factor,
+        margin: 15 
+    } 
+}; 
+   
